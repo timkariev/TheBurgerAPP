50d05aeb86895131454e4fb82364a41c0ce25b4d
timkariev <tim.kariev95@gmail.com>
2018-09-26 22:08:41 -0700

    update1.4

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (name) VALUES ('Veggie Burger');
 INSERT INTO burgers (name) VALUES ('Double Bacon Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Chicken Avocado Burger');
+INSERT INTO burgers (name) VALUES ('Tripple Meaty Burger');
 
